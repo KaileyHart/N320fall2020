dcec6bb25ce70a99d3d4bdf2d5808fd8cc594659
Kailey <kalyhart@iu.edu>
2020-10-15 13:18:55 -0400

    Update vue listing

---
+++ vuedatalistinglab/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+<!--
+DONE: Create your own JSON data structure for a collection of your own. ( acorns? One Piece Figures? Ms. Marvel comics? Guitars? )
+DONE: Create a Vue application using either the CLI or Codesandbox
+
+Load the data into the application and pass the data into a component
+Display the data in that component in a v-for list
+-->
+  <div id="app">
+    
+
+    <Authors v-bind:authors="this.authors"  />
+   
+  </div>
+</template>
+
+<script>
+
+import Authors from './components/Authors.vue'
+
+export default {
+  name: 'App',
+  components: {
+   
+    Authors
+  },
+  data: function ()  {
+    return {
+      authors: null,
+   
+     
+    }
+  },
+  mounted: function () {
+    fetch("data/data.json")
+    .then((response) => response.json())
+    .then(json => {
+      this.authors = json;
+      console.log(json);
+    })
+    .catch((err) => 
+    {
+      console.log(err)
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuedatalistinglab/src/components/Authors.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1>
+      AUTHORS<br />
+      AND <br />MY FAVORITE BOOKS BY THEM.
+    </h1>
+
+    <div>
+      <div v-bind:key="author.id" v-for="author in authors" class="author">
+        <p><strong>Author Name:</strong> {{ author.authorName }}</p>
+        <p><strong>Still Alive?</strong> {{ author.alive }}</p>
+        <p><strong>Favorite Book:</strong> {{ author.bookName }}</p>
+        <p><strong>Book Publish Date:</strong> {{ author.publishDate }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Authors",
+  props: ["authors"],
+
+  data: function() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: #000;
+  text-shadow: 3px 2px 2px #7c7c7c;
+}
+
+.author {
+  margin: 0 auto;
+  border-radius: 5px;
+  border: 1px solid #000;
+  box-shadow: 8px 10px 8px #7c7c7c;
+  margin-top: 30px;
+  max-width: 350px;
+  padding: 20px;
+}
+
+.author p {
+  color: #000;
+  text-align: left;
+}
+</style>
